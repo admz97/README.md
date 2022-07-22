8165d6f20c4a257b419cd076bb068c1f9d0f5145
admz97 <109818267+admz97@users.noreply.github.com>
2022-07-22 21:09:14 +0300

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -3,17 +3,17 @@ import translator
 
 	def unit_test():
 	    test_case = [
-	        "Hello, My name is TackHyun Jung",  # correct input
-	        "i love black pink",  # correct input
+	        "Hello, My name is Ahmed",  # correct input
+	        "i love football",  # correct input
 	        "have a nice day",  # correct input
 	        "'Hello",  # test for the translation of the world 'Hello
 	        "",  # test for null input
-	        "123456"]  # number input
+	        "1234"]  # number input
 	
 
 	    for case in test_case:
-	        german_text = translator.Translation().englishtogerman(case)
-	        print(german_text)
+	        french_text = translator.Translation().englishtofrench(case)
+	        print(french_text)
 	
 
 	if __name__ == '__main__':

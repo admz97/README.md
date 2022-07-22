0d622079f9d284700df98c0e5c6ac51dec4bb3e6
admz97 <109818267+admz97@users.noreply.github.com>
2022-07-22 20:48:47 +0300

    Update translator1.py

--- translator1.py
+++ translator1.py
@@ -1,29 +1,27 @@
 class ahmed_translation:
 def __init__ (self):
-""" input(english) >>>> print(french)"""
-authenticator = IAMAuthenticator('yh3FZLY_WgCVgtIDnS0G6e1fG-OJK0d08DtZ4utRQ0vr')
-self. language_translator = LanguageTranslatorV3(
-version='2021-03-16°,
-authenticator=authenticator
-)
-                               
-self. language_translator.set_service_url(
-'https://api.us-south.Language-translator.watson.cloud.ibm.com/instances/1b2ac512-fc0e-4960-bf35-ffe5380de3011')
+  """ input(english) >>>> print(french)"""
+  authenticator = IAMAuthenticator('yh3FZLY_WgCVgtIDnS0G6e1fG-OJK0d08DtZ4utRQ0vr')
+  self. language_translator = LanguageTranslatorV3(
+    version='2021-03-16°,
+    authenticator = authenticator
+  )
+  self.language_translator.set_service_url(
+    'https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/68838cb3-b18b-4011-8a7e-56d7a2ef6158')
 
 
 def englishtofrench(self, english-text):
-""" input(engLish) -> return(french) """
-translation_result = self.Language_translator,translate(
-  text=english_text,
-  model_id='en-fr').get_result()
-
+  """ input(engLish) -> return(french) """
+  translation_result = self.Language_translator,translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+  
 french text = translation_ result['translations'][0]['translation']
 return french_text
 
 
 if __name__ == '__main__'
-
-english_text = "HelLo, My name is Ahmed
-french_text = ahmed_translation().engLishtofrench(engLish_text)
-print (french_text)
+  english_text = "HelLo, My name is Ahmed
+  french_text = ahmed_translation().engLishtofrench(engLish_text)
+  print(french_text)
 
